6760cd792be68bd3fac3f8d133fa04b8903dc5a7
YuyaIsh <dame.blazer@gmail.com>
2023-05-21 10:12:13 +0900

    動くバージョンに戻す

--- paid_instead.py
+++ paid_instead.py
@@ -3,10 +3,8 @@ import psycopg2
 import datetime
 import pandas as pd
 from dateutil.relativedelta import relativedelta
-import os
 
 def main():
-    st.write(os.environ["USERNAME"])
     persons = [st.secrets["person1"],st.secrets["person2"]]
 
     st.subheader("生活費計算")
@@ -98,7 +96,6 @@ def main():
         with col_display:
             pay_history_df = pay_history_df.set_index("id")
             st.dataframe(pay_history_df.iloc[::-1],height=250)
-
 
 def conn_supabase():
     ip = st.secrets["host"]
